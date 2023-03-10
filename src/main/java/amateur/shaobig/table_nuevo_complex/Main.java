37d19d8da99c5f8865a6c86fee10e450ee6924e9
shaobig <54940461+shaobig@users.noreply.github.com>
2023-03-10 15:53:29 +0200

    TNC-001: Bootstrap the minimal set of program dependencies for the first launch

---
+++ src/main/java/amateur/shaobig/table_nuevo_complex/Main.java
@@ -0,0 +1,13 @@
+package amateur.shaobig.table_nuevo_complex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
